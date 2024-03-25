d538a0a6f770f9f769ef084227fa6e6dc3c940e2
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-25 09:34:05 -0700

    fixed errors

--- frontend/src/pages/landingpage/landingpage.jsx
+++ frontend/src/pages/landingpage/landingpage.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/navbar'
-import Hero from '../../components/hero/hero'
-import Overview from '../../components/overview/overview'
-import Workexperience from '../../components/workexperience/workexperience'
-import Skills from '../../components/skills/skills'
-import Projects from '../../components/projects/projects'
-import Testimonials from '../../components/testimonials/testimonials'
-import Contact from '../../components/contact/contact'
+import React from "react";
+import Navbar from "../../components/navbar/navbar"
+import Hero from "../../components/hero/hero";
+import Overview from "../../components/overview/overview";
+import Workexperience from "../../components/workexperience/workexperience";
+import Skills from "../../components/skills/skills";
+import Projects from "../../components/projects/projects";
+import Testimonials from "../../components/testimonials/testimonials";
+import Contact from "../../components/contact/contact";
 
 const Landingpage = () => {
   return (
     <div>
-        <Navbar/>
-        <Hero/>
-        <Overview/>
-        <Workexperience/>
-        <Skills/>
-        <Projects/>
-        <Testimonials/>
-        <Contact/>
+      <Navbar />
+      <Hero />
+      <Overview />
+      <Workexperience />
+      <Skills />
+      <Projects />
+      <Testimonials />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default Landingpage
+export default Landingpage;
