b81c055b94018ac9e254922b118f2a7ccbbfbb78
Trey_cie <kusiwaahboateng1997@gmail.com>
2024-03-28 06:21:45 -0700

    changes 1:19

--- frontend/src/pages/blogs/article.jsx
+++ frontend/src/pages/blogs/article.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Article = () => {
-  const location =  useLocation()
+  const location = useLocation();
+  const post = location.state;
+
+  return (
+    <div className='text-black'>
+      <div className="max-w-2xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+
+        
+        {post.img && (
+          <div className="mb-4">
+            <img src={post.img} alt={post.title} className="w-full" />
+          </div>
+        )}
+
+        
+        <div>
+          <p className="text-lg">{post.content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
 
 
-const post = location.state
+
+
+
+
+// import React from 'react'
+// import { useLocation } from 'react-router-dom'
+
+// const Article = () => {
+//   const location =  useLocation()
+
+
+// const post = location.state
 
    
-return (
-  <div className='text-black'>
-    <div className="max-w-2xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
-      {post.img && (
-        <img src={post.img} alt={post.title} className="w-full mb-4" />
-      )}
-      <p className="text-lg">{post.content}</p>
-    </div>
-  </div>
-)
-}
-export default Article
+// return (
+//   <div className='text-black'>
+//     <div className="max-w-2xl mx-auto">
+//       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+//       {post.img && (
+//         <img src={post.img} alt={post.title} className="w-full mb-4" />
+//       )}
+//       <p className="text-lg">{post.content}</p>
+//     </div>
+//   </div>
+
+  
+// )
+// }
+
+// export default Article
