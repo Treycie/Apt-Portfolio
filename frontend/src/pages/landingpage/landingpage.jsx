867b4c43068a9e17fd4e5c244ea764b8582f4b4e
darleynartey <159792141+darleynartey@users.noreply.github.com>
2024-03-28 10:28:41 +0000

    Darley work (#8)
    * changes made
    
    * Changes to the landing page
    
    * Changes to overview
    
    * Change made to landing page
    
    * change contact
    
    * Change made to contact
    
    * Changes made to hero
    
    * Changes made to work experiences
    
    ---------
    
    Co-authored-by: Treycie <136967369+Treycie@users.noreply.github.com>

--- frontend/src/pages/landingpage/landingpage.jsx
+++ frontend/src/pages/landingpage/landingpage.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import Header from "../../components/header/header";
 import Navbar from "../../components/navbar/navbar"
 import Hero from "../../components/hero/hero";
 import Overview from "../../components/overview/overview";
 import Workexperience from "../../components/workexperience/workexperience";
-import Skills from "../../components/skills/skills";
+//import Skills from "../../components/skills/skills";
 import Projects from "../../components/projects/projects";
-import Testimonials from "../../components/testimonials/testimonials";
+//import Testimonials from "../../components/testimonials/testimonials";
 import Contact from "../../components/contact/contact";
+import "../../../src/index.css"
 
 const Landingpage = () => {
   return (
-    <div>
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+      <Header/>
       <Navbar />
       <Hero />
       <Overview />
       <Workexperience />
-      <Skills />
       <Projects />
-      <Testimonials />
       <Contact />
     </div>
   );
